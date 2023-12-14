2d34cd98b8a7d757a6a986c338f301dc71d55399
I Kingsley <>
2023-12-14 14:59:31 +0100

    no message

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const blogRoutes = require('./routes/blogRoutes'); // Ensure this path is correct
+const db = require('./database');  // Ensure this path is correct to your database setup file
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));// Body parser for form data
+app.use('/public', express.static('public'));
+// Routes
+app.get('/', (req, res) => res.redirect('/blogs')); // Redirect root to the blogs
+app.use('/blogs', blogRoutes);
+
+// Database initialization (optional here if you have it in your database.js but in our example we have it so you can leave this as a comment)
+
+db.run("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)");
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
